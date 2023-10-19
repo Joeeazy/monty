259246a054e841662ec693e11d7867c80dd1c300
Joeeazy <joeeazy34@gmail.com>
2023-10-19 18:22:07 +0300

    errors fix

--- main.h
+++ main.h
@@ -1,10 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
